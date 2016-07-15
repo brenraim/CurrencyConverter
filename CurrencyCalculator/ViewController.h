a8a42790926cd9ef9d4d3ecbd8a4e0c28d94bc7c
Brendan <braimann2017@pingry.org>
2016-07-15 10:45:34 -0400

    Initial Commit

---
+++ CurrencyCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CurrencyCalculator
+//
+//  Created by Brendan Raimann on 7/15/16.
+//  Copyright © 2016 Brendan Raimann. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CurrencyCalculator/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CurrencyCalculator
+//
+//  Created by Brendan Raimann on 7/15/16.
+//  Copyright © 2016 Brendan Raimann. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+   [super viewDidLoad];
+   // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+   [super didReceiveMemoryWarning];
+   // Dispose of any resources that can be recreated.
+}
+
+@end
